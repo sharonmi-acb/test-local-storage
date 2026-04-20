133bb1365197bd75a60a11d47ae7b21750154b8b
Sharon Mishayev <Sharonmi@accessibe.com>
2026-04-20 13:17:32 +0300

    Update file paths for stylesheets and navigation links in HTML and JavaScript files for consistent routing

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const checkAuthButton = document.getElementById('check-auth-button');
 if (checkAuthButton) {
   checkAuthButton.addEventListener('click', () => {
     const authValue = window.localStorage.getItem('auth');
-    window.location.href = authValue === '1234' ? '/ok' : '/error';
+    window.location.href = authValue === '1234' ? 'ok/index.html' : 'error/index.html';
   });
 }
